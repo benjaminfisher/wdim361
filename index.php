9087f1e5e5bceb7938059d65bf90a99b59eecca3
Benjamin Fisher <neohell@hotmail.com>
2012-04-03 14:39:53 -0700

    PHP echo and print commands

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?php echo "Hello world!"; ?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<?php 
+		echo "</br> This is an echo.";
+	?>
+	
+	<p>This is pure HTML</p>
+	
+	<?php print 'This is more php'; ?>
+</body>
+</html>
