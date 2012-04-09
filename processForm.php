8a987ecd9d97c4cf7e81cd9c1870d3e20462dc68
Benjamin Fisher <neohell@hotmail.com>
2012-04-09 02:43:38 -0700

    Changes to the form

--- processForm.php
+++ processForm.php
@@ -1,21 +1,23 @@
 <?php
-	$program = $_POST['program'];
-	$pYear = $_POST['programYear'];
-	$housingType = $_POST['housing'];
-	$travelType = $_POST['travelType'];
-	$cDistance = $_POST['commuteDistance'];
-	$useFrequency = $_POST['useFrequency'];
-	$usePeriod = $_POST['usePeriod'];
-	$pAccess = $_POST['primaryAccess'];
-	$sAccess = $_POST['secondaryAccess'];
-	$siteComments = $_POST['siteComments'];
-	$contact = $_POST['contact']; // Boolean for user testing contact
-	$sName = $_POST['studentName'];
-	$sPhone = $_POST['studentPhoneNumber']; 
-	$sEmail = $_POST['studentEmailAddress'];
+	@$role = $_POST['userRole'];
+	@$program = $_POST['program'];
+	@$pYear = $_POST['programYear'];
+	@$housingType = $_POST['housing'];
+	@$travelType = $_POST['travelType'];
+	@$cDistance = $_POST['commuteDistance'];
+	@$useFrequency = $_POST['useFrequency'];
+	@$usePeriod = $_POST['usePeriod'];
+	@$pAccess = $_POST['primaryAccess'];
+	@$sAccess = $_POST['secondaryAccess'];
+	@$siteComments = $_POST['siteComments'];
+	@$contact = $_POST['contact'] || FALSE; // Boolean for user testing contact
+	@$cName = $_POST['contactName'];
+	@$cPhone = $_POST['contactPhoneNumber']; 
+	@$cEmail = $_POST['contactEmailAddress'];
 	
 	date_default_timezone_set('America/Los_Angeles');
-
+	
+	var_dump($_REQUEST);
 ?>
 <!DOCTYPE HTML>
 <html lang="en-US">
@@ -24,16 +26,15 @@
 	<title>Form Processing</title>
 </head>
 <body>
-	<p><?php echo date('l F j, Y g:h a'); ?></p>
+	<p><strong>Survey Submitted:</strong> <?php echo date('l F j, Y g:h a'); ?></p>
 	
 	<p>
-	<?php if ($phone && strlen($phone) < 10){ 
+	<?php if ($contact == "on" && strlen($cPhone) < 10){ 
 		echo "Phone number entered is invalid. It must be at least 10 digits.";
-	} else {
-		echo "First: $fName, Last: $lName, Email: $email, Phone: $phone";
+	} else if (){
+		
 	}
 	?>
-	
 	</p>
 </body>
 </html>
