2bde000bc0e9fa7503c9f9529ed12550163f2c01
Benjamin Fisher <neohell@hotmail.com>
2012-04-16 02:56:28 -0700

    Week 3 inclass files from video

---
+++ week3/inclass/_header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title><?php if(!empty($title)){ echo $title; } ?></title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	
+	<link rel="stylesheet" type="text/css" href="style.css" media="all" />
+	<link rel="stylesheet" type="text/css" href="form.css" media="all" />
+	
+	<?php
+		if(!empty($header_extras)){
+			print $header_extras;
+		}
+	?>
+</head>
+<body>
+	<div class="wrapper">
---
+++ week3/inclass/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$title = 'Week 3 PHP';
+include '_header.php';
+
+?>
+
+<form action="" method="post">
+	
+	
+</form>
+
+<?php include '_footer.php'; ?>
