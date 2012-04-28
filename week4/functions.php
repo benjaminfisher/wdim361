a66c6e2681354bfa84be3baf8ed771d5ebcaaacd
Benjamin Fisher <neohell@hotmail.com>
2012-04-28 03:13:28 -0700

    Working on the tag_wrap function

--- week4/functions.php
+++ week4/functions.php
@@ -1,14 +1,32 @@
 <?php 
 
+function is_block($string){
+	
+	$block_elements = array('article', 'aside', 'blockquote', 'button', 'canvas', 'caption', 'col', 'colgroup', 
+	'dd', 'div', 'dl', 'dt', 'embed', 'fieldset', 'figcaption', 'figure', 'footer', 'form', 'p', 'ol', 'li');
+	
+	if(is_string($string)){
+		if(array_search($string, $block_elements)){
+			return true;
+		} else {
+			return false;
+		}
+	} else {
+		return false;
+	};
+};
+
 function page_name($path){
 	return ucwords(strstr($path, '.', TRUE));
 };
 
 // Return $content wrapped in a specified HTML $tag with optional $class
 function tag_wrap($tag, $content, $class = NULL){
-	$result = "<$tag";
+	$result = (is_block($tag)) ? "\n<" : "<" ;
+	$result .= $tag;
 	$result .= (!empty($class)) ? ' class="'.$class.'">' : '>' ;
-	$result .= "$content</$tag>\n";
+	$result .= $content;
+	$result .= (is_block($tag)) ? "\n</$tag>\n" : "</$tag>" ;
 	
 	return $result;
 };
--- week4/index.php
+++ week4/index.php
@@ -11,6 +11,8 @@ $page .= tag_wrap('li', 'third');
 $page = tag_wrap('ol', $page);
 
 echo $page;
+echo tag_wrap('span', 'This is not a block.');
+echo tag_wrap('span', 'This is not a block.');
 
 include '_footer.php';
 ?>
