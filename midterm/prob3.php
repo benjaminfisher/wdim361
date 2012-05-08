7240785fd477c7a90d332f40196cd4adfce33032
Benjamin Fisher <neohell@hotmail.com>
2012-05-08 14:55:40 -0700

    Problem 3 complete.

--- midterm/prob3.php
+++ midterm/prob3.php
@@ -10,16 +10,40 @@ include '_header.php';
 
 // Create products array
 
-$product_price = array(
-    'boots' =>  78.95,
-    'hats'  =>  42.95,
-    'jeans' =>  28.50,
-    'shirts'=>  23.45
-);
+function calculate_order_total($order){
+    $product_price = array(
+        'boots' =>  78.95,
+        'hats'  =>  42.95,
+        'jeans' =>  28.50,
+        'shirts'=>  23.45
+    );
+    
+    // Compute the total of the order
+    
+    $total = ($order['boots'] * $product_price['boots'])
+        + ($order['hats'] * $product_price['hats'])
+        + ($order['jeans'] * $product_price['jeans'])
+        + ($order['shirts'] * $product_price['shirts']);
+       
+    // Return the result
+    return $total;
+}
+
 
 // Collect user input for quantities of products
 
-// Compute order total
+if(!empty($_POST)){
+    $order = array();
+    
+    $order['boots'] = cln($_POST['order_boots_count']);
+    $order['hats'] = cln($_POST['order_hats_count']);
+    $order['jeans'] = cln($_POST['order_jeans_count']);
+    $order['shirts'] = cln($_POST['order_shirts_count']);
+    
+    // Compute order total
+    $order_total = calculate_order_total($order);
+
+}
 
 ?>
 
@@ -39,6 +63,9 @@ $product_price = array(
 
 <?php else: ?>
 <!-- Output the order total formatted as currency -->
+
+<p>Thanx Cowboy!</p>
+<p>Your order total is: $<?php echo number_format($order_total, 2, '.', ',') ?></p>
     
 <?php endif; ?>
 
