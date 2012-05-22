9cb504e5f1cf801d58229b08024eb241fe770274
Benjamin Fisher <neohell@hotmail.com>
2012-05-22 13:01:43 -0700

    Switched home page to use _nav.php. Added the linkedinapi.html

---
+++ _nav.php
@@ -0,0 +1,19 @@
+<?php
+$navigation = array(
+    'Week 1'    =>  'week1/',
+    'Week 2'    =>  'week2/',
+    'Week 3'    =>  'week3/',
+    'Week 4'    =>  'week4/',
+    'Week 5'    =>  'week5/'
+);
+
+// Print out the navigation items and links from $navigation
+echo "<nav>\n";
+echo "\t<ul>\n";
+foreach ($navigation as $key => $value) {
+    echo "\t<li><a href=\"$value\">$key</a></li>\n";
+}
+echo "\t</ul>\n";
+echo "</nav>\n";
+
+?>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>WDIM361 Spring 2012 | Server Side Scripting</title>
+</head>
+<body>
+	<h1>WDIM361 Spring 2012 | Server Side Scripting</h1>
+	
+	<?php include '_nav.php'; ?>
+</body>
+</html>
